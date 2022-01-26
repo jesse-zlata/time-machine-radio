a50ecf6492f02c8c62bcc8314027f8f3e7afff13
Jesse Zlata <jesse.zlata@warbyparker.com>
2022-01-26 09:26:13 -0500

    Prototype testing

---
+++ main.py
@@ -0,0 +1,21 @@
+import vlc
+import os
+import random
+
+def main():
+    media_paths = get_media_paths("audio")
+    media_list = vlc.MediaList(media_paths)
+    media_player = vlc.MediaPlayer()
+    media_item = media_list.item_at_index(random.randrange(0, media_list.count()-1,1))
+    media_player.set_media(media_item)
+    media_player.play()
+    # need to add the below so the program does not terminate while audio is playing
+    # this will be replaced by a wait on gpio input
+    input("Press Enter to end...")
+
+def get_media_paths(media_folder: str) -> list[str]:
+    file_names = os.listdir(media_folder)
+    return [f"{media_folder}/{file_name}" for file_name in file_names]
+
+if __name__ == "__main__":
+    main()
