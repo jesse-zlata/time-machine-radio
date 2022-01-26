bf6d674003ce769143c49a7f90589f3d2ad78c43
Jesse Zlata <jesse.zlata@warbyparker.com>
2022-01-26 17:51:51 -0500

    Add rotor and print values

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import vlc
 import os
 import random
 
+from gpiozero import RotaryEncoder
+
 def main():
     media_paths = get_media_paths("audio")
     media_list = vlc.MediaList(media_paths)
     media_player = vlc.MediaPlayer()
+    rotor = RotaryEncoder(21, 20, max_steps=media_list.count())
+
+    print(rotor.values)
+
     media_item = media_list.item_at_index(random.randrange(0, media_list.count()-1,1))
     media_player.set_media(media_item)
     media_player.play()
@@ -16,6 +22,7 @@ def main():
 def get_media_paths(media_folder: str) -> list[str]:
     file_names = os.listdir(media_folder)
     return [f"{media_folder}/{file_name}" for file_name in file_names]
+
 
 if __name__ == "__main__":
     main()
