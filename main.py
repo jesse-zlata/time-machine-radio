14f1b428695810a30ffeb504d200c4a2f53896ff
Jesse Zlata <jesse.zlata@gmail.com>
2022-01-27 22:02:43 +0000

    Use button in place of encoder while waiting on parts

--- main.py
+++ main.py
@@ -2,26 +2,43 @@ import vlc
 import os
 import random
 
-from gpiozero import RotaryEncoder
+from threading import Event
+from gpiozero import Device, RotaryEncoder, LED, Button
 
 def main():
     media_paths = get_media_paths("audio")
     media_list = vlc.MediaList(media_paths)
     media_player = vlc.MediaPlayer()
-    rotor = RotaryEncoder(21, 20, max_steps=media_list.count())
+    btn = Button(14, pull_up=False)
+    #rotor = RotaryEncoder(15, 14, wrap=True, max_steps=media_list.count())
+    #rotor.steps = -1 * media_list.count()
+    #print(rotor.steps)
+    #print(rotor.value)
+    done = Event()
 
-    print(rotor.values)
+    def change_media_file():
+        #file_index = (rotor.steps + media_list.count()) / (2 * media_list.count())
+        media_player.stop()
+        media_item = media_list.item_at_index(random.randrange(0, media_list.count()-1,1))
+        media_player.set_media(media_item)
+        media_player.play()
 
-    media_item = media_list.item_at_index(random.randrange(0, media_list.count()-1,1))
-    media_player.set_media(media_item)
-    media_player.play()
-    # need to add the below so the program does not terminate while audio is playing
-    # this will be replaced by a wait on gpio input
-    input("Press Enter to end...")
+    def stop_script():
+        done.set()
+
+
+    btn.when_released = change_media_file
+    btn.when_held = stop_script
+    #rotor.when_rotated = change_media_file
+    #media_player.set_media(media_item)
+    #media_player.play()
+    done.wait()
+    #rotor.wait_for_rotate()
 
 def get_media_paths(media_folder: str) -> list[str]:
     file_names = os.listdir(media_folder)
     return [f"{media_folder}/{file_name}" for file_name in file_names]
+
 
 
 if __name__ == "__main__":
