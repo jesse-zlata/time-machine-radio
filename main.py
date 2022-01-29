e39151320cb81ceaeabbdaed799abf81e272369e
Jesse Zlata <jesse.zlata@gmail.com>
2022-01-29 01:24:16 +0000

    Added the encoder, button, and LED

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
+#!/usr/bin/env python3
+
 import vlc
 import os
-import random
 
 from threading import Event
-from gpiozero import Device, RotaryEncoder, LED, Button
+from gpiozero import RotaryEncoder, Button, LED
 
 def main():
-    media_paths = get_media_paths("audio")
+    print("Running")
+    # To run the script on start up I call it from rc.local
+    # This requires I put the full path to the audio folder in code
+    media_paths = get_media_paths("/home/pi/time-machine-radio/audio")
     media_list = vlc.MediaList(media_paths)
     media_player = vlc.MediaPlayer()
-    btn = Button(14, pull_up=False)
-    #rotor = RotaryEncoder(15, 14, wrap=True, max_steps=media_list.count())
-    #rotor.steps = -1 * media_list.count()
-    #print(rotor.steps)
-    #print(rotor.value)
-    done = Event()
+    btn = Button(18, pull_up=False)  # The encoder has a built in button
+    led = LED(23)  # Ready LED
+    rotor = RotaryEncoder(15, 14, wrap=True, max_steps=media_list.count())
+    done = Event()  # Using the threading lib to keep the program running
 
     def change_media_file():
-        #file_index = (rotor.steps + media_list.count()) / (2 * media_list.count())
+        print("Change File")
+        print(rotor.steps)
         media_player.stop()
-        media_item = media_list.item_at_index(random.randrange(0, media_list.count()-1,1))
+        media_item = media_list.item_at_index(abs(rotor.steps))
+        print(media_item)
         media_player.set_media(media_item)
         media_player.play()
 
-    def stop_script():
-        done.set()
+    def stop_audio():
+        print("Pause")
+        media_player.stop()
 
 
-    btn.when_released = change_media_file
-    btn.when_held = stop_script
-    #rotor.when_rotated = change_media_file
-    #media_player.set_media(media_item)
-    #media_player.play()
+    btn.when_released = stop_audio
+    rotor.when_rotated = change_media_file
+    led.on()  # Debugging LED will light if program boots fully
     done.wait()
-    #rotor.wait_for_rotate()
 
 def get_media_paths(media_folder: str) -> list[str]:
     file_names = os.listdir(media_folder)
